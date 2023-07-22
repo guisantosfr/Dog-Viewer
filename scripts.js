accc94806827ce73648246057e6e38ce8558fb3d
Guilherme Santos <santosgui678@gmail.com>
2023-07-22 11:49:47 -0300

    Reset to first version

--- scripts.js
+++ scripts.js
@@ -1,64 +1,37 @@
 const LIST_URL = 'https://dog.ceo/api/breeds/list/all';
 
 const selectElement = document.querySelector('.breeds');
-const selectSubBreedElement = document.querySelector('.sub-breeds');
 const dogImage = document.querySelector('.dog-img');
 const spinnerElement = document.querySelector('.spinner');
-let listAllBreeds = null;
-let breedSelected = null;
-let hasSubBreedSelected = false;
 
-fetch(LIST_URL) //retorna uma promise contendo a resposta (objeto Response)
-  .then(response => response.json()) //extrai o corpo JSON da resposta
+fetch(LIST_URL)
+  .then(response => response.json())
   .then(data => {
-    const breedsArray = Object.keys(data.message); //obtem as chaves do objeto
-    fillOptions(breedsArray, selectElement);
-    listAllBreeds = data.message;
+    const breedsArray = Object.keys(data.message);
+    fillOptions(breedsArray);
   });
 
 selectElement.addEventListener('change', e => {
-  const breed = e.target.value.toLowerCase(); //obtem a opção atual do select
-  hasSubBreedSelected = false;
-  breedSelected = breed;
-
-  getDogImage(`https://dog.ceo/api/breed/${breed}/images/random`, breed)
-});
-
-selectSubBreedElement.addEventListener('change', e => {
   const breed = e.target.value.toLowerCase();
-  hasSubBreedSelected = true;
 
-  getDogImage(`https://dog.ceo/api/breed/${breedSelected}/${breed}/images/random`)
+  getDogImage(`https://dog.ceo/api/breed/${breed}/images/random`)
 });
 
-function fillOptions(options, element) {
-  element.replaceChildren();
-
-  options.forEach(breed => {
-    const [firstLetter, ...rest] = breed;
-    const capitalizedBreedName = firstLetter.toUpperCase() + rest.join('').toLowerCase();
+function fillOptions(array) {
+  array.forEach(breed => {
+    const capitalizedBreedName = breed.replace(/\b\w/g, l => l.toUpperCase());
 
     const optionElement = document.createElement('option');
     optionElement.innerHTML = `<option value=${breed}>${capitalizedBreedName}</option>`;
 
-    element.appendChild(optionElement);
+    selectElement.appendChild(optionElement);
   });
-}
-
-function displaySubBreeds() {
-  if (!listAllBreeds[breedSelected].length) {
-    return selectSubBreedElement.parentNode.classList.remove('show');
-  }
-
-  selectSubBreedElement.parentNode.classList.add('show');
-  fillOptions(listAllBreeds[breedSelected], selectSubBreedElement);
 }
 
 function getDogImage(url) {
   fetch(url) //busca imagem aleatoria na api
     .then(response => response.json())
     .then(img => {
-      hasSubBreedSelected ? null : displaySubBreeds();
       spinnerElement.classList.add('show');
       dogImage.classList.remove('show');
 
@@ -70,3 +43,18 @@ function getDogImage(url) {
     dogImage.classList.add('show');
   });
 }
+
+function fetchInitialImage() {
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => {
+      const imageUrl = data.message;
+      const imgElement = document.querySelector('.dog-img');
+      imgElement.src = imageUrl;
+    })
+    .catch(error => {
+      console.error('Error fetching image data:', error);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', fetchInitialImage);
